1b0ef38300c7f3f01ec18c0290b440d2f96b7009
san.ank007 <san.ank007@gmail.com>
2020-05-31 00:55:26 +0530

    Added LIbrary EmployeeManagement.BAL
    added for writing business logic.

---
+++ EmployeeManagementCore/EmployeeManagement.BAL/Repository/EmployeeRepository/EmployeeRepository.cs
@@ -0,0 +1,92 @@
+﻿
+using EmployeeManagement.DAL.DataAccessRepository;
+using EmployeeManagement.Models.Employees;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace EmployeesManagement.BAL.Repository.EmployeeRepository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+
+        IEmployeeDataAccess _employeeDataAccess = null;
+        public EmployeeRepository(IEmployeeDataAccess employeeDataAccess)
+        {
+            _employeeDataAccess = employeeDataAccess;
+
+        }
+        public async Task<bool> CreateEmployeeAsync(Employee employeetoCreate)
+        {
+            var result =await _employeeDataAccess.CreateEmployeeAsync(employeetoCreate);
+            if (result==1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }    
+            
+        }
+
+        public async Task<Dictionary<bool,string>> DeleteEmployeeAsync(int id)
+        {
+            Dictionary<bool, string> responseDic = new Dictionary<bool, string>();
+            var employee=await GetEmployeeAsync(id);
+            if (employee == null)
+            {
+                responseDic.Add(false, "No Record Found");
+
+            }
+            else
+            {
+            var result= await _employeeDataAccess.DeleteEmployeeAsync(id);
+                if (result == 1)
+                    responseDic.Add(true, "Record Deleted");
+                else if(result==0)
+                    responseDic.Add(false, "No Record Found");
+                else
+                    responseDic.Add(false, "Exception Raised");
+            }
+
+            return responseDic;
+
+        }
+
+        public async Task<List<Employee>> GetAllEmployeeAsync()
+        {
+            return await _employeeDataAccess.GetEmployees();
+        }
+
+        public async Task<Employee> GetEmployeeAsync(int id)
+        {
+            return await _employeeDataAccess.GetEmployeeAsync(id);
+        }
+
+        public async Task<Dictionary<bool,string>> UpdateEmployeeAsync(int id, Employee employeetoUpdate)
+        {
+            Dictionary<bool, string> responseDic = new Dictionary<bool, string>();
+            var employee = await GetEmployeeAsync(id);
+            if (employee == null)
+            {
+                responseDic.Add(false, "No Record Found");
+
+            }
+            else
+            {
+                var result = await _employeeDataAccess.UpdateEmployeeAsync(id,employeetoUpdate);
+                if (result==1)
+                    responseDic.Add(true, "Record Updated");
+                else if(result==0)
+                    responseDic.Add(false, "No Record Found");
+                else
+                    responseDic.Add(false, "Exception Raised");
+
+            }
+
+            return responseDic;
+        }
+    }
+}
---
+++ EmployeeManagementCore/EmployeeManagement.BAL/Repository/EmployeeRepository/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Models.Employees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EmployeesManagement.BAL.Repository.EmployeeRepository
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<Employee>> GetAllEmployeeAsync();
+        Task<Employee> GetEmployeeAsync(int id);
+        Task<bool> CreateEmployeeAsync(Employee employeetoCreate);
+        Task<Dictionary<bool, string>> UpdateEmployeeAsync(int id, Employee employeetoCreate);
+        Task<Dictionary<bool,string>> DeleteEmployeeAsync(int id);
+
+    }
+}
