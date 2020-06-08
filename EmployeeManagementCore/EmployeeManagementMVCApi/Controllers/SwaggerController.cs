256c1fea3c385847f2d36c985bdec5e0bf1958e6
san.ank007 <san.ank007@gmail.com>
2020-06-09 04:43:34 +0530

    Added in .NET CORE 3.1 MVC project

---
+++ EmployeeManagementCore/EmployeeManagementMVCApi/Controllers/SwaggerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementMVCApi.Controllers
+{
+    public class SwaggerController : Controller
+    {
+        public IActionResult Index()
+        {
+            var route = HttpContext.Request.Host;
+            string baseUrl = HttpContext.Request.GetEncodedUrl().Replace("Swagger","swagger");
+            string swaggerUrl = baseUrl + "/index.html";
+            return Redirect(swaggerUrl);
+        }
+    }
+}
